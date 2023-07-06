9d6e1681fee0f9270042ebba0da1c04010c661d0
Eagles Solution <eaglessolution2022@gmail.com>
2023-07-06 19:48:38 +0200

    Add existing file

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="sr">
+    <head>
+        <!--====== Required meta tags ======-->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!--====== Title ======-->
+        <title>Eagles Solution</title>
+        <!--====== Favicon Icon ======-->
+        <link rel="shortcut icon" href=" {{ asset('assets/images/favicon.ico') }}" type="image/png">
+        <!--====== Default css ======-->
+        <link rel="stylesheet" href=" {{ asset('assets/css/default.css') }}">
+        <!--====== Style css ======-->
+        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+        <!--====== Responsive css ======-->
+        <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+    </head>
+    <body>
+        <!--====== offcanvas-panel ======-->
+        <div class="offcanvas-panel">
+            <div class="panel-overlay"></div>
+            <div class="offcanvas-panel-inner">
+                <div class="panel-logo">
+                    <a href="index.html"><img src="assets/images/logo/logo-1.png" alt="Qempo"></a>
+                </div>
+                <div class="about-us">
+                    <h5 class="panel-widget-title">About Us</h5>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore ipsam alias quae cupiditate quas, neque eum magni impedit asperiores.
+                    </p>
+                </div>
+                <div class="contact-us">
+                    <h5 class="panel-widget-title">Contact Us</h5>
+                    <ul>
+                        <li>
+                            <i class="fal fa-map-marker-alt"></i>
+                            121 King St, Melbourne VIC 3000, Australia.
+                        </li>
+                        <li>
+                            <i class="fal fa-envelope-open"></i>
+                            <a href="mailto:hello@barky.com"> hello@barky.com</a>
+                            <a href="mailto:info@barky.com">info@barky.com</a>
+                        </li>
+                        <li>
+                            <i class="fal fa-phone"></i>
+                            <a href="tel:(312)-895-9800">(312) 895-9800</a>
+                            <a href="tel:(312)-895-9888">(312) 895-9888</a>
+                        </li>
+                    </ul>
+                </div>
+                <a href="#" class="panel-close"><i class="fal fa-times"></i></a>
+            </div>
+        </div><!--====== offcanvas-panel ======-->
+        <!--====== Start Header Section ======-->
+        <header class="theme-header transparent-header">
+            <!-- header Navigation -->
+            <div class="header-navigation navigation-style-v1">
+                <div class="nav-overlay"></div>
+                <div class="container-fluid">
+                    <div class="primary-menu">
+                        <div class="site-branding">
+                            <a href="index.html" class="brand-logo"><img src="assets/images/logo/logo-1.png" alt="Site Logo"></a>
+                        </div>
+                        <div class="nav-menu">
+                            <!-- Navbar Close -->
+                            <div class="navbar-close"><i class="far fa-times"></i></div>
+                            <!-- Nav Search -->
+                            <div class="nav-search">
+                                <form>
+                                    <div class="form_group">
+                                        <input type="email" class="form_control" placeholder="Search Here" name="email" required>
+                                        <button class="search-btn"><i class="fas fa-search"></i></button>
+                                    </div>
+                                </form>
+                            </div>
+                            <!-- Main Menu -->
+                            <nav class="main-menu">
+                                <ul>
+                                    <li class="menu-item has-children"><a href="#" class="active nav-link">Demos</a>
+                                        <ul class="sub-menu">
+                                            <li class="has-children"><a href="#">Multipage</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="index.html">Creative Agency</a></li>
+                                                    <li><a href="index-2.html">Digital Agency</a></li>
+                                                    <li><a href="index-3.html">Design Studio</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="has-children"><a href="#">Onepage</a>
+                                                <ul class="sub-menu">
+                                                    <li><a href="onepage-index-1.html">Creative Agency</a></li>
+                                                    <li><a href="onepage-index-2.html">Digital Agency</a></li>
+                                                    <li><a href="onepage-index-3.html">Design Studio</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item has-children"><a href="#" class="nav-link">Services</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="services.html">Our Service</a></li>
+                                            <li><a href="service-details.html">Service Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item has-children"><a href="#" class="nav-link">Portfolio</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="projects.html">Our Portfolio</a></li>
+                                            <li><a href="project-details.html">Portfolio Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item has-children"><a href="#" class="nav-link">Pages</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="about.html">About Us</a></li>
+                                            <li><a href="team.html">Our Team</a></li>
+                                            <li><a href="team-details.html">Team Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item has-children"><a href="#" class="nav-link">Blog</a>
+                                        <ul class="sub-menu">
+                                            <li><a href="blogs.html">Our Blog</a></li>
+                                            <li><a href="blog-details.html">Blog Details</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="menu-item"><a href="contact.html" class="nav-link">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="header-right-nav">
+                            <ul>
+                                <li class="bar-item"><a href="#"><img src="assets/images/dot.png" alt="dot"></a></li>
+                                <li class="navbar-toggle-btn">
+                                    <div class="navbar-toggler">
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header><!--====== End Header Section ======-->
